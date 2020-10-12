04a0a495290438c2b42dc809426622fc37631ae2
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-12 11:35:26 +0200

    Add more prompts to Driver class

--- Driver.java
+++ Driver.java
@@ -9,14 +9,31 @@ class Driver{
         System.out.println("3. Exit");
 
         Scanner command = new Scanner(System.in);
-        System.out.println("Enter the number of your choice and press Enter: ");
+        System.out.println("Choice: ");
 
         int choice = command.nextInt();
 
         if(choice == 1){
             System.out.println("You selected Linear Data Structures");
         }else if(choice == 2){
-            System.out.println("You selected Non-linear Data Structures");
+            System.out.println("Please chose the Non-linear Data Structure you would like to generate questions for from the list below:");
+            System.out.println("1. Binary Search Tree");
+            System.out.println("2. Hash Tables");
+            System.out.println("3. Priority Queues");
+            System.out.println("0. Exit");
+            System.out.println("Choice:");
+            choice = command.nextInt();
+
+            if(choice == 1){
+                System.out.println("1. Binary Search Tree");
+            }else if(choice == 2){
+                System.out.println("2. Hash Tables");
+            }else if(choice == 3){
+                System.out.println("3. Priority Queues");
+            }else if(choice == 0){
+                System.exit(0);
+            }
+
         }else if(choice == 0){
             System.out.println("Goodbye!");
         }
