71b476ab62322947df48fa63ef8b88f38bfdd4dd
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-15 23:11:57 +0200

    Comment code

---
+++ Node.java
@@ -0,0 +1,11 @@
+//Binary Node tree for the BST class
+//Author: Sabir Buxsoo
+class Node { 
+    int data; 
+    Node left, right; 
+      
+    Node(int d) { 
+        data = d; 
+        left = right = null; 
+    } 
+} 
