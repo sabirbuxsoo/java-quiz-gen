f558735e5e80fd01f18d0bd7d9c672c78721f15c
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-16 04:23:32 +0200

    Refactor Driver class

--- Driver.java
+++ Driver.java
@@ -1,41 +1,34 @@
+import java.io.IOException;
 import java.util.Scanner;
 
-class Driver{
-    public static void main(String args[]){
+class Driver {
+    public static void main(String args[]) throws IOException {
         System.out.println("~~~ Welcome to Data Structures Quiz Generator ~~~");
-        System.out.println("The following options are available:");
-        System.out.println("1. Linear Data Structures");
-        System.out.println("2. Non-linear Data Structures");
-        System.out.println("3. Exit");
+        
 
         Scanner command = new Scanner(System.in);
+        System.out.println("Please chose the Data Structure you would like to generate questions for from the list below:");
+        System.out.println("1. Binary Search Tree");
+        System.out.println("2. Hash Tables");
+        System.out.println("3. Priority Queues");
+        System.out.println("0. Exit");
+        
         System.out.println("Choice: ");
 
         int choice = command.nextInt();
 
+        choice = command.nextInt();
+
         if(choice == 1){
-            System.out.println("You selected Linear Data Structures");
-        }else if(choice == 2){
-            System.out.println("Please chose the Non-linear Data Structure you would like to generate questions for from the list below:");
             System.out.println("1. Binary Search Tree");
+        }else if(choice == 2){
             System.out.println("2. Hash Tables");
+            Hashtable x = new Hashtable();
+            x.genMain();
+        }else if(choice == 3){
             System.out.println("3. Priority Queues");
-            System.out.println("0. Exit");
-            System.out.println("Choice:");
-            choice = command.nextInt();
-
-            if(choice == 1){
-                System.out.println("1. Binary Search Tree");
-            }else if(choice == 2){
-                System.out.println("2. Hash Tables");
-            }else if(choice == 3){
-                System.out.println("3. Priority Queues");
-            }else if(choice == 0){
-                System.exit(0);
-            }
-
         }else if(choice == 0){
-            System.out.println("Goodbye!");
+            System.exit(0);
         }
 
     }
