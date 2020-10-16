184cb489aff145c48ded9cef1779b2fdb087eab7
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-16 04:26:48 +0200

    Remove unnecessary variables

--- Node.java
+++ Node.java
@@ -1,9 +1,13 @@
 //Binary Node tree for the BST class
 //Author: Sabir Buxsoo
 class Node { 
-    int data, height, color; 
+    int data, height; 
     Node left, right; 
       
+    Node(){
+
+    }
+
     Node(int d) { 
         data = d; 
         left = right = null; 
