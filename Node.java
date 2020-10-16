6f5532e2bebe9602ab9474b839756dafc0da1a1a
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-16 03:08:59 +0200

    Add color variable for RBT implementation

--- Node.java
+++ Node.java
@@ -1,11 +1,12 @@
 //Binary Node tree for the BST class
 //Author: Sabir Buxsoo
 class Node { 
-    int data; 
+    int data, height, color; 
     Node left, right; 
       
     Node(int d) { 
         data = d; 
         left = right = null; 
+        height = 1;
     } 
 } 
