a3a822bfbba6f647473e3da9b201f6bfaa4e9402
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-17 02:41:16 +0200

    Add options

--- Driver.java
+++ Driver.java
@@ -3,35 +3,50 @@ import java.util.Scanner;
 
 class Driver {
     public static void main(String args[]) throws IOException {
+        int choice;
         System.out.println("~~~ Welcome to Data Structures Quiz Generator ~~~");
-        
-
         Scanner command = new Scanner(System.in);
-        System.out.println("Please chose the Data Structure you would like to generate questions for from the list below:");
-        System.out.println("1. Binary Search Tree");
-        System.out.println("2. AVL Tree");
-        System.out.println("3. Hashtable");
-        System.out.println("4. Priority Queue");
-        System.out.println("5. Create Test");
-        System.out.println("0. Exit");
-        
-        System.out.println("Choice: ");
+        while(true){            
+            System.out.println("Please chose the Data Structure you would like to generate questions for from the list below:");
+            System.out.println("1. Binary Search Tree");
+            System.out.println("2. AVL Tree");
+            System.out.println("3. Hashtable");
+            System.out.println("4. Priority Queue");
+            System.out.println("5. Create Test");
+            System.out.println("0. Exit");
+            System.out.println("Choice: ");      
+            
+            while (true){
+                choice = command.nextInt();
 
-        int choice = command.nextInt();
+                if(choice == 1){
+                    System.out.println("########### Binary Search Tree ###########");
+                    BSTQuestion bstQuestion = new BSTQuestion(command);
+                    bstQuestion.promptUser();
+                }else if(choice == 2){
+                    System.out.println("2. Hash Tables");
+                    Hashtable x = new Hashtable();
+                    x.genMain();
+                }else if(choice == 3){
+                    System.out.println("3. Priority Queues");
+                }else if(choice == 0){
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                }
 
-        choice = command.nextInt();
+                System.out.println("########### What would you like to do next? ###########");
+                System.out.println("0. Exit");
+                System.out.println("9. Go back to main menu");
+                System.out.println("Choice: ");
 
-        if(choice == 1){
-            System.out.println("########### Binary Search Tree ###########");
-        }else if(choice == 2){
-            System.out.println("2. Hash Tables");
-            Hashtable x = new Hashtable();
-            x.genMain();
-        }else if(choice == 3){
-            System.out.println("3. Priority Queues");
-        }else if(choice == 0){
-            System.exit(0);
-        }
-
+                choice = command.nextInt();
+                if(choice == 0){
+                    System.out.println("Goodbye!");
+                    System.exit(0);
+                }else if(choice == 9){
+                    break;
+                }
+            }   
+        }  
     }
 }
