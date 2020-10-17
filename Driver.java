12bc69e1339f71d9a8d8cf1884f51fb7b2038d69
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-17 07:39:48 +0200

    Update style

--- Driver.java
+++ Driver.java
@@ -1,40 +1,68 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * This is the main Driver class which allows interaction with the program from the CLI
+ * The class takes input form the user and performs various operations (generates questions)
+ * Author: Sabir Buxsoo (BXSMUH001)
+ */
+
 class Driver {
     public static void main(String args[]) throws IOException {
         int choice;
-        System.out.println("~~~ Welcome to Data Structures Quiz Generator ~~~");
+        System.out.println("############################################################################");
+        System.out.println("###########                                                      ###########");
+        System.out.println("###########      Welcome to Data Structures Quiz Generator       ###########");
+        System.out.println("###########                                                      ###########");
+        System.out.println("############################################################################");
+        System.out.println();
         Scanner command = new Scanner(System.in);
         while(true){            
-            System.out.println("Please chose the Data Structure you would like to generate questions for from the list below:");
+            System.out.println("Please chose the Data Structure from the list below:");
+            System.out.println();
             System.out.println("1. Binary Search Tree");
             System.out.println("2. AVL Tree");
             System.out.println("3. Hashtable");
             System.out.println("4. Priority Queue");
             System.out.println("5. Create Test");
             System.out.println("0. Exit");
-            System.out.println("Choice: ");      
+            System.out.println();
+            System.out.println("Ener Choice: ");      
             
             while (true){
                 choice = command.nextInt();
 
                 if(choice == 1){
-                    System.out.println("########### Binary Search Tree ###########");
+                    System.out.println("############################################################################");
+                    System.out.println("###########                                                      ###########");
+                    System.out.println("###########                 Binary Search Tree                   ###########");
+                    System.out.println("###########                                                      ###########");
+                    System.out.println("############################################################################");
+                    System.out.println();
                     BSTQuestion bstQuestion = new BSTQuestion(command);
                     bstQuestion.promptUser();
                 }else if(choice == 2){
-                    System.out.println("2. Hash Tables");
-                    Hashtable x = new Hashtable();
-                    x.genMain();
+                    System.out.println("############################################################################");
+                    System.out.println("###########                                                      ###########");
+                    System.out.println("###########                      AVL Tree                        ###########");
+                    System.out.println("###########                                                      ###########");
+                    System.out.println("############################################################################");
+                    System.out.println();
+                    AVLQuestion avlQuestion = new AVLQuestion(command);
+                    avlQuestion.promptUser();
                 }else if(choice == 3){
-                    System.out.println("3. Priority Queues");
+                    System.out.println("4. Priority Queues");
                 }else if(choice == 0){
                     System.out.println("Goodbye!");
                     System.exit(0);
                 }
 
-                System.out.println("########### What would you like to do next? ###########");
+                System.out.println("############################################################################");
+                System.out.println("###########                                                      ###########");
+                System.out.println("###########           What would you like to do next?            ###########");
+                System.out.println("###########                                                      ###########");
+                System.out.println("############################################################################");
+                System.out.println();
                 System.out.println("0. Exit");
                 System.out.println("9. Go back to main menu");
                 System.out.println("Choice: ");
@@ -45,6 +73,8 @@ class Driver {
                     System.exit(0);
                 }else if(choice == 9){
                     break;
+                }else{
+                    System.out.println("Option unavailable. You will be redirected to the Main Menu");
                 }
             }   
         }  
