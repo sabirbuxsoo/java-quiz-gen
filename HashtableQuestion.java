39292ee67e145f2e99a5c9db66e734196727b9cd
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-18 10:22:39 +0200

    Add constructor

--- HashtableQuestion.java
+++ HashtableQuestion.java
@@ -1,10 +1,12 @@
-import java.security.SecureRandom;
 import java.util.*;
-import java.io.*;
 
 public class HashtableQuestion extends AnswerHelper
 {
     Scanner scan;
+
+    HashtableQuestion(){
+        
+    }
     HashtableQuestion(Scanner scan){
         this.scan = scan;
     }
@@ -21,15 +23,63 @@ public class HashtableQuestion extends AnswerHelper
         }
 
         String point = "Question (" + points + " point)";
-        String insertionQuestion = "What is the resulting hashtable after inserting the following elements "
-                + Arrays.toString(array) + " in a hashtable of length " + hashtable.maxSize
-                + " using the linear probing scheme and the hash function is h(x) = x % " + hashtable.maxSize + " ?";
+        String insertionQuestion = "What is the resulting hashtable after inserting the keys "
+        + Arrays.toString(array) + " into an initially empty hashtable of length " + hashtable.getMaxSize()
+        + " using open addressing with hash function is h(x) = x % " + hashtable.getMaxSize() + " and linear probing ?";
         String ans = generateHashtableAnswers(hashtable.toString()); // Generate Answers
         String questionOne = point + "\n" + insertionQuestion + "\n" + ans;
         hashtable.clear();
 
         return questionOne;
     }
+
+    static String two(String points) {
+        int modVal = 10;
+        Hashtable hashtable = new Hashtable(modVal);
+        ArrayGenerator arrayGen = new ArrayGenerator();
+
+        String[] array = Arrays.stream(arrayGen.getArray()).mapToObj(String::valueOf).toArray(String[]::new);
+
+
+        for (int i = 0; i < array.length; i++) {
+            hashtable.put(array[i]);
+        }
+
+        String point = "Question (" + points + " point)";
+        String insertionQuestion = "What is the resulting hashtable after inserting the keys "
+        + Arrays.toString(array) + " into an initially empty hashtable of length " + hashtable.getMaxSize()
+        + " using open addressing with hash function is h(x) = x % " + hashtable.getMaxSize() + " and linear probing ?";
+        String ans = generateHashtableAnswers(hashtable.toString()); // Generate Answers
+        String questionTwo = point + "\n" + insertionQuestion + "\n" + ans;
+        hashtable.clear();
+
+        return questionTwo;
+    }
+
+
+    static String three(String points) {
+        int modVal = 10;
+        Hashtable hashtable = new Hashtable(modVal);
+        ArrayGenerator arrayGen = new ArrayGenerator();
+
+        String[] array = Arrays.stream(arrayGen.getArray()).mapToObj(String::valueOf).toArray(String[]::new);
+
+
+        for (int i = 0; i < array.length; i++) {
+            hashtable.put(array[i]);
+        }
+
+        String point = "Question (" + points + " point)";
+        String insertionQuestion = "What is the resulting hashtable after inserting the keys "
+                + Arrays.toString(array) + " into an initially empty hashtable of length " + hashtable.getMaxSize()
+                + " using open addressing with hash function is h(x) = x % " + hashtable.getMaxSize() + " and linear probing ?";
+        String ans = generateHashtableAnswers(hashtable.toString()); // Generate Answers
+        String questionTwo = point + "\n" + insertionQuestion + "\n" + ans;
+        hashtable.clear();
+
+        return questionTwo;
+    }
+
 
     // Method to Generate questions from user selection. Method also takes number of
     // questions to be generated
@@ -43,7 +93,7 @@ public class HashtableQuestion extends AnswerHelper
                 break;
             case 2:
                 for (int i = 0; i < numQuestions; i++) {
-                    finalAnswers += one(points);
+                    finalAnswers += two(points);
                 }
                 break;
             default:
@@ -54,7 +104,7 @@ public class HashtableQuestion extends AnswerHelper
     }
 
     void promptUser(){
-        System.out.println("Select question to generate:\n1. Insert values into hash table with linear probing\n2. Delete value from hashtable\n");
+        System.out.println("Select question to generate:\n1. Insert values into hash table with linear probing h(k) = x % 7\n2. Insert values into hash table with linear probing h(k) = x % 10\n");
         System.out.print("Enter question selection and press Enter: ");
         int selection = scan.nextInt();
         System.out.print("Enter number of questions you want generated and press Enter: ");
@@ -63,6 +113,7 @@ public class HashtableQuestion extends AnswerHelper
         String points = scan.next();
         System.out.print("Enter output file name to save questions (do not add extension): ");
         String fileName = scan.next();
+       
         generateQuestions(numQuestions, selection, fileName, points);
     }
 }
