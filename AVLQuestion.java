c84e9f3aabdb52c25b281b027dc22da4a34b8ce2
bxsmuh001 <bxsmuh001@cs.uct.ac.za>
2020-10-18 10:21:11 +0200

    Add constructor

--- AVLQuestion.java
+++ AVLQuestion.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
  */
 public class AVLQuestion extends AnswerHelper {
     Scanner scan;
+    AVLQuestion(){
+        
+    }
     AVLQuestion(Scanner scan){
         this.scan = scan;
     }
